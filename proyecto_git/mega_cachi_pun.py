89e3c3fa1ec2a316323cfa6901dc367f574bca63
M1guelits <miguel.wenborne@estudiante.uc.cl>
2026-03-26 13:02:12 -0300

    debilidades

---
+++ proyecto_git/mega_cachi_pun.py
@@ -0,0 +1,12 @@
+print("Bienvenido al mega cachi-pun")
+
+roca_debilidades = [papel, spock]
+
+lagarto_debilidades = [roca, tijeras]
+
+spock_debilidades = [lagarto, papel]
+
+tijeras_debilidades = [roca, spock]
+
+papel_debilidades = [tijeras, lagarto]
+
