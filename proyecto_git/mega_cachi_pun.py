a8bdff1056241ee0772bf0f362c29685c7d1cc2f
vicenteolg <vicenteolg@estudiante.uc.cl>
2026-03-26 22:58:43 -0300

    arreglo caso empate

--- proyecto_git/mega_cachi_pun.py
+++ proyecto_git/mega_cachi_pun.py
@@ -16,34 +16,40 @@ papel_debilidades = ["tijeras", "lagarto"]
 lista_movimientos = ["roca", "papel", "tijeras", "lagarto", "spock"]
 
 def jugar(movimiento):
+    if movimiento == "roca" and random.choice(lista_movimientos)=="roca":
+        return "empate"
     if movimiento == "roca" and random.choice(lista_movimientos) not in roca_debilidades:
         print("VICTORIA: Aplastante!" )
         return True
     elif movimiento == "roca" and random.choice(lista_movimientos) in roca_debilidades:
         print("DERROTA: Pierde roca")
         return False
-
+    if movimiento == "lagarto" and random.choice(lista_movimientos)=="lagarto":
+        return "empate"
     if movimiento == "lagarto" and random.choice(lista_movimientos) not in lagarto_debilidades:
         print("VICTORIA: Ataque reptil")
         return True
     elif movimiento == "lagarto" and random.choice(lista_movimientos) in roca_debilidades:
         print("DERROTA: Masacrado a sangre fría")
         return False
-
+    if movimiento == "spock" and random.choice(lista_movimientos)=="spock":
+        return "empate"
     if movimiento == "spock" and random.choice(lista_movimientos) not in spock_debilidades:
         print("VICTORIA: Dedos separados") 
         return True
     elif movimiento == "spock" and random.choice(lista_movimientos) in spock_debilidades:
         print("DERROTA: Devuelvete a Star-Trek")
         return False
-
+    if movimiento == "tijeras" and random.choice(lista_movimientos)=="tijeras":
+        return "empate"
     if movimiento == "tijeras" and random.choice(lista_movimientos) not in tijeras_debilidades:
         print("VICTORIA: Tajadura") 
         return True
     elif movimiento == "tijeras" and random.choice(lista_movimientos) in tijeras_debilidades:
         print("DERROTA: Quebrantado") 
         return False
-
+    if movimiento == "papel" and random.choice(lista_movimientos)=="papel":
+        return "empate"
     if movimiento == "papel" and random.choice(lista_movimientos) not in papel_debilidades:
         print("VICTORIA: Envoltura")
         return True
@@ -54,11 +60,13 @@ def jugar(movimiento):
 puntaje_yo=0
 puntaje_terminal=0
 while True:
-    move=input("Elige entre roca, lagarrto, spock, tijeras o papel")
+    move=input("Elige entre roca, lagarto, spock, tijeras o papel -> ")
     if jugar(move)==True:
         puntaje_yo+=1
     elif jugar(move)==False:
         puntaje_terminal+=1
+    elif jugar(move)=="empate":
+        print("EMPATE")
     if puntaje_terminal==10:
         print("HA GANADO LA TERMINAL")
         break
